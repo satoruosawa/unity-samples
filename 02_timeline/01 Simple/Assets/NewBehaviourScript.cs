0f3a4ce441ad748a725e8379f914e1ff63a3bebb
Satoru Osawa <osawa@takram.com>
2020-02-19 17:48:01 +0900

    Added Script animation

--- 02_timeline/01 Simple/Assets/NewBehaviourScript.cs
+++ 02_timeline/01 Simple/Assets/NewBehaviourScript.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class NewBehaviourScript : MonoBehaviour {
+	[SerializeField] Vector3 position = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = position;
+	}
 }
